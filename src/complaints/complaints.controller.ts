15dd4b6d3d92fdd97ed6e69da4394a5e125368ec
conceptiabello <conceptiabello@gmail.com>
2025-06-07 20:25:10 +0200

    correction

--- src/complaints/complaints.controller.ts
+++ src/complaints/complaints.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ComplaintsService } from './complaints.service';
+
 
 @Controller('complaints')
-export class ComplaintsController {}
+export class ComplaintsController {
+     constructor(private readonly complaintsService: ComplaintsService) {}
+
+  @Get('filter')
+  filterComplaints(
+    @Query('zone') zone?: string,
+    @Query('categorie') categorie?: string,
+    @Query('statut') statut?: string,
+    @Query('dateDebut') dateDebut?: string,
+    @Query('dateFin') dateFin?: string,
+  ) {
+    return this.complaintsService.filterComplaints({
+      zone,
+      categorie,
+      statut,
+      dateDebut,
+      dateFin,
+    });
+  }
+}
--- src/complaints/complaints.service.ts
+++ src/complaints/complaints.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ComplaintsService {}
+export class ComplaintsService {
+    filterComplaints(arg0: { zone: string | undefined; categorie: string | undefined; statut: string | undefined; dateDebut: string | undefined; dateFin: string | undefined; }) {
+        throw new Error('Method not implemented.');
+    }
+}
